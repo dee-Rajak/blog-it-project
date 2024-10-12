0eedc82c0687798821277abf5f2dc0eb6178e916
Saurav1-2 <sk5741467@gmail.com>
2024-10-12 12:50:16 +0530

    migrations are changed, had to push

---
+++ Back-End/API/BlogItAPI/BlogItAPI/Migrations/20241011151626_Second Mig with all functions.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogItAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class SecondMigwithallfunctions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Authors_CommentAuthorId",
+                table: "Comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_CommentAuthorId",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "CommentAuthorId",
+                table: "Comments");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AuthorId",
+                table: "Comments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "AuthorId1",
+                table: "Comments",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_AuthorId1",
+                table: "Comments",
+                column: "AuthorId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Authors_AuthorId1",
+                table: "Comments",
+                column: "AuthorId1",
+                principalTable: "Authors",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Authors_AuthorId1",
+                table: "Comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_AuthorId1",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "AuthorId1",
+                table: "Comments");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AuthorId",
+                table: "Comments",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CommentAuthorId",
+                table: "Comments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_CommentAuthorId",
+                table: "Comments",
+                column: "CommentAuthorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Authors_CommentAuthorId",
+                table: "Comments",
+                column: "CommentAuthorId",
+                principalTable: "Authors",
+                principalColumn: "Id");
+        }
+    }
+}
